e2c75450510a14804c8d47b4b913a03df2f98713
Katrina Behnken <katrina.behnken@gmail.com>
2020-12-08 19:56:38 -0700

    Add routes

---
+++ routes/recipeRoutes.js
@@ -0,0 +1,35 @@
+const recipes = require('../controllers/recipeController');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({
+    storage: storage
+});
+
+module.exports = app => {
+    // Create a new recipe
+    app.post('/api/v1/recipes', upload.single('imageFile'), recipes.create);
+
+    // Return all recipes
+    app.get('/api/v1/recipes', recipes.findAll);
+
+    // Return a single Recipe with recipeId
+    app.get('/api/v1/recipes/:recipeId', recipes.findByRecipeId);
+
+    // Return all recipes with userId
+    // app.get('/api/v1/recipes-by-author/:userId', recipes.findByUserId);
+
+    // Update a recipe with recipeId
+    // app.put('/api/v1/recipes/:recipeId', upload.single('imageFile'), recipes.update);
+
+    // // Delete a recipe with recipeId
+    // app.delete('/api/v1/recipes/:recipeId', recipes.delete);
+
+    // // Return recently added recipes
+    // app.get('/api/v1/recent-recipes', recipes.findRecentRecipes);
+
+    // // Return an image with recipeId
+    // app.get('/api/v1/recipes/photos/:recipeId', recipes.findPhoto);
+
+    // // Return search results
+    // app.post('/api/v1/search-recipes', recipes.findSearchResults);
+};
---
+++ routes/userRoutes.js
@@ -0,0 +1,22 @@
+module.exports = app => {
+    const users = require('../controllers/userController.js');
+
+    // Create a new user profile
+    app.post('/api/v1/users', users.create);
+
+    // Return all user profiles
+    // app.get('/api/v1/users', users.findAll);
+
+    // Return a single user profile with userId
+    // app.get('/api/v1/users/:userId', users.findOne);
+
+    // Update a user profile with userId
+    // app.put('/api/v1/users/:userId', users.update);
+
+    // Delete a user profile with userId
+    // app.delete('/api/v1/users/:userId', users.delete);
+
+    // Update a user password with userId
+    // app.put('/api/v1/change-password', users.updatePassword);
+
+};
