f28f7cccb3318f239df78c87098290b88927a3fe
Katrina Behnken <katrina.behnken@gmail.com>
2021-01-10 18:19:05 -0700

    Update MONGODB_OPTIONS

--- dbConfig/db.js
+++ dbConfig/db.js
@@ -2,7 +2,9 @@ const mongoose = require('mongoose');
 const { MONGODB_SERVER, MONGODB_PORT } = process.env;
 const MONGODB_OPTIONS = {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
 };
 mongoose.connect('mongodb://' + MONGODB_SERVER + ':' + MONGODB_PORT + '/recipeBox', MONGODB_OPTIONS)
 .then(() => {
